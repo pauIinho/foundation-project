ce24d04d6cb10c8f3fe2a76a6e6e5fa13be4d71a
Pavel Teplyuk <xakerpaul@gmail.com>
2017-11-28 13:40:57 +0700

    implemented creation of order from cart

--- src/AppBundle/Controller/Cabinet/OrderController.php
+++ src/AppBundle/Controller/Cabinet/OrderController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller\Cabinet;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -33,5 +34,42 @@ class OrderController extends Controller
         return $this->render('@App/Cabinet/Order/orders_cart.html.twig', [
             'order' => $order
         ]);
+    }
+
+    /**
+     * @Route("/cabinet/orders/create", name="create_order")
+     * @Method({"POST"})
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createAction(Request $request)
+    {
+        $user = $this->getUser();
+
+        if ('ward' !== $user->getType()) {
+            throw new AccessDeniedHttpException('Функционал недоступен данному типу пользователя');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $wardRepository = $em->getRepository('AppBundle\Entity\Ward');
+        $ward = $wardRepository->findOneBy(['user' => $user]);
+
+        $orderId = $request->request->get('order');
+        $orderRepository = $em->getRepository('AppBundle\Entity\Order');
+        $order = $orderRepository->findOneBy(['ward' => $ward, 'id' => (int) $orderId, 'status' => 0]);
+
+        if (null !== $order) {
+            $order->setStatus(1);
+            $em->persist($order);
+            $em->flush();
+            $this->addFlash('success', 'Заявка успешно создана');
+
+            return $this->redirectToRoute('orders_cart');
+        }
+
+        $this->addFlash('error', 'Не удается создать заявку');
+        return $this->redirectToRoute('orders_cart');
     }
 }
