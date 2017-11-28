87b0b95e865e447bf416983fddb18889a3f729f7
Pavel Teplyuk <xakerpaul@gmail.com>
2017-11-28 10:35:44 +0700

    implemented updating profile changes

--- src/AppBundle/Controller/Cabinet/ProfileController.php
+++ src/AppBundle/Controller/Cabinet/ProfileController.php
@@ -3,14 +3,19 @@
 namespace AppBundle\Controller\Cabinet;
 
 use AppBundle\Entity\User;
+use FOS\UserBundle\Model\UserManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class ProfileController extends Controller
 {
     /**
      * @Route("/cabinet/profile", name="cabinet_profile")
+     * @Method({"GET"})
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
@@ -45,5 +50,134 @@ class ProfileController extends Controller
                 ]);
                 break;
         }
+
+        return null;
+    }
+
+    /**
+     * @Route("/cabinet/profile/update", name="cabinet_profile_update")
+     * @Method({"POST"})
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function updateAction(Request $request)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $form = $request->request->all();
+        $em = $this->getDoctrine()->getManager();
+
+        try {
+            switch ($user->getType()) {
+                case 'contributor':
+                    $changesCount = 0;
+                    $contributorRepository = $em->getRepository('AppBundle\Entity\Contributor');
+                    $contributor = $contributorRepository->findOneBy(['user' => $user]);
+
+                    if ($contributor === null) {
+                        throw new \Exception('Произошла непредвиденная ошибка');
+                    }
+
+                    if (isset($form['full_name']) && !empty($form['full_name']) &&
+                        $contributor->getName() !== $form['full_name']) {
+
+                        $contributor->setName($form['full_name']);
+                        $changesCount++;
+                    }
+
+                    if (isset($form['organization']) && !empty($form['organization']) &&
+                        $contributor->getOrganizationName() !== $form['organization']) {
+
+                        $contributor->setOrganizationName($form['organization']);
+                        $changesCount++;
+                    }
+
+                    if (isset($form['contact_phone']) && !empty($form['contact_phone']) &&
+                        $contributor->getContactPhone() !== $form['contact_phone']) {
+
+                        $contributor->setContactPhone($form['contact_phone']);
+                        $changesCount++;
+                    }
+
+                    if ($changesCount > 0) {
+                        $em->persist($contributor);
+                        $em->flush();
+                    } else {
+                        throw new Exception('Изменения отсутствуют!');
+                    }
+
+                    break;
+                case 'ward':
+                    $changesCount = 0;
+                    $wardRepository = $em->getRepository('AppBundle\Entity\Ward');
+                    $ward = $wardRepository->findOneBy(['user' => $user]);
+
+                    if ($ward === null) {
+                        throw new \Exception('Произошла непредвиденная ошибка');
+                    }
+
+                    if (isset($form['full_name']) && !empty($form['full_name']) &&
+                        $ward->getContactFullname() !== $form['full_name']) {
+
+                        $ward->setContactFullname($form['full_name']);
+                        $changesCount++;
+                    }
+
+                    if (isset($form['organization']) && !empty($form['organization']) &&
+                        $ward->getName() !== $form['organization']) {
+
+                        $ward->setName($form['organization']);
+                        $changesCount++;
+                    }
+
+                    if (isset($form['address']) && !empty($form['address']) &&
+                        $ward->getAddress() !== $form['address']) {
+
+                        $ward->setAddress($form['address']);
+                        $changesCount++;
+                    }
+
+                    if (isset($form['contact_phone']) && !empty($form['contact_phone']) &&
+                        $ward->getContactPhone() !== $form['contact_phone'] ) {
+
+                        $ward->setContactPhone($form['contact_phone']);
+                        $changesCount++;
+                    }
+
+                    if ($changesCount > 0) {
+                        $em->persist($ward);
+                        $em->flush();
+                    } else {
+                        throw new Exception('Изменения отсутствуют!');
+                    }
+
+                    break;
+            }
+
+            if (isset($form['email']) && !empty($form['email']) && $user->getEmail() !== $form['email']) {
+                $userRepository = $em->getRepository('AppBundle\Entity\User');
+                $isEmailExists = (bool) $userRepository->findOneBy(['email' => $form['email']]);
+
+                if ($isEmailExists) {
+                    throw new Exception('Пользователь с таким email уже существует');
+                }
+
+                $user->setEmail($form['email']);
+                $user->setUsername($form['email']);
+            }
+            if (isset($form['password']) && !empty($form['password'])) {
+                $user->setPlainPassword($form['password']);
+            }
+            $em->persist($user);
+            $em->flush();
+
+        } catch (\Exception $exception) {
+            $this->addFlash('error', $exception->getMessage());
+            return $this->redirectToRoute('cabinet_profile');
+        }
+
+        $this->addFlash('success', 'Данные успешно сохранены');
+        return $this->redirectToRoute('cabinet_profile');
     }
 }
